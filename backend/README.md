e396fe6fb7b945d3b7b9d373fdd8eaf50ed64d7c
Nick Maclean <nick@imaclean.me>
2026-09-03 11:26:56 -0500

    setup server endpoints

---
+++ backend/README.md
@@ -0,0 +1,69 @@
+# Leaderboard Backend
+
+We've got a plain-old php server here in the backend.
+
+## Endpoints
+
+### /scores
+
+#### GET
+
+Get the top ten scores from the leaderboard right now.
+
+Example output: {
+    "scores": [
+        {
+            "initials": "NIC",
+            "score":4820,
+            "created_at":"2026-09-03T15:37:43Z"
+        }
+    ]
+}
+
+#### POST
+
+Insert a new score for the leaderboard, then get information about the update.
+
+Input: {
+    "initials": "IAN",
+    "score": 25000,
+    "nonce": 32 char nonce,
+    "signature": 64 char HMAC of 'initials:score:nonce'
+}
+
+Output: {
+    "scores": [
+        {
+            "initials": "IAN",
+            "score": 25000,
+            "created_at":"2026-09-03T15:37:43Z"
+        }
+    ],
+    "accepted": true,
+    "rank": 1
+}
+
+## Setup
+
+1. Copy config.php:
+
+```
+cp config.php.example config.php
+```
+
+2. In config.php, replace `CHANGE-ME!` with a real random secret:
+
+```
+php -r "echo bin2hex(random_bytes(32));"
+```
+
+3. Run the server
+
+```
+php -S localhost:8080 -t public
+```
+
+## Notes
+
+HMAC signing only deters casual tatmpering since the secret will also need to ship with the client binary.
+
--- backend/public/index.php
+++ backend/public/index.php
@@ -1,8 +1,92 @@
 <?php
-  require("../config.php");
-  include("../src/db.php");
+require("../config.php");
+include("../src/db.php");
+include("../src/signature.php");
 
-  $config = get_config();
-  $db = new Database($config);
 
-?>
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); 
+$method = $_SERVER['REQUEST_METHOD'];
+header('Content-Type: application/json; charset=utf-8'); // we always send back json
+
+$config = get_config();
+$db = new Database($config);
+
+
+if ($path == '/scores')
+{
+  if ($method == 'GET')
+  {
+    echo json_encode([
+      'scores' => $db->getTop()
+    ]);
+  }
+  elseif ($method == 'POST')
+  {
+    $data = json_decode(file_get_contents('php://input'), true);
+    if (!is_array($data))
+    {
+      http_response_code(400);
+      exit;
+    }
+
+    // validate json input
+    $initials = $data['initials'] ?? null;
+    $score = $data['score'] ?? null;
+    $nonce = $data['nonce'] ?? null;
+    $signature = $data['signature'] ?? null;
+    if (!is_string($initials) || !ctype_alnum($initials) || strlen($initials) != 3)
+    {
+      http_response_code(400);
+      exit;
+    }
+    if (!is_int($score) || $score < 0)
+    {
+      http_response_code(400);
+      exit;
+    }
+    if (!is_string($nonce) || strlen($nonce) !== 32 || !ctype_xdigit($nonce)
+        || !is_string($signature) || strlen($signature) !== 64 || !ctype_xdigit($signature))
+    {
+      http_response_code(400);
+      exit;
+    }
+
+    // check nonce and signature
+    $replay = $db->nonceExists($nonce);
+    if ($replay)
+    {
+      http_response_code(400);
+      exit;
+    }
+
+    $valid = Signature::verify($initials, $score, $nonce, $signature, $config['secret']);
+    if (!$valid)
+    {
+      http_response_code(400);
+      exit;
+    }
+
+    // perform insertion and get data to return
+    $db->recordNonce($nonce);
+    $initials = strtoupper($initials);
+    $id = $db->insert($initials, $score);
+    $rank = $db->rankFor($id);
+    $scores = $db->getTop();
+
+    // send success response!
+    echo json_encode([
+      'accepted' => true,
+      'rank' => $rank,
+      'scores' => $scores,
+    ]);
+  }
+  else
+  {
+    http_response_code(404);
+  }
+}
+else
+{
+  http_response_code(404);
+}
+
